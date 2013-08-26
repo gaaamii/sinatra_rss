d8ddef8e5dc752a1e024a652c56eec74206d99a3
gaaamii <i1shi4ga4mi1@gmail.com>
2013-08-27 04:16:01 +0900

    インデント修正した

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	@rss = RSS::Parser.parse("http://feeds.feedburner.com/hatena/b/hotentry")
-	@entries = @rss.items
-	erb :index
+  @rss = RSS::Parser.parse("http://feeds.feedburner.com/hatena/b/hotentry")
+  @entries = @rss.items
+  erb :index
 end
