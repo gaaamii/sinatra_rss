e1763cba34fb791e3f656fd0c7aabfb2cca7d966
gaaamii <i1shi4ga4mi1@gmail.com>
2013-08-27 03:44:12 +0900

    third commit

--- main.rb
+++ main.rb
@@ -1,13 +1,7 @@
 require 'rss'
-=begin
 require 'sinatra'
 require 'sinatra/reloader'
-=end
 
-@rss = RSS::Parser.parse("http://feeds.feedburner.com/hatena/b/hotentry")
-p @rss[0]
-
-__END__
 get '/' do
 	@rss = RSS::Parser.parse("http://feeds.feedburner.com/hatena/b/hotentry")
 	@entries = @rss.items
